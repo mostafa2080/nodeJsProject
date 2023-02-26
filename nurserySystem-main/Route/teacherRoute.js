4412a6bbe2a402e82a92facefab4fc83ea8b82fe
mostafa2080 <mostafashaaban2080@gmail.com>
2023-02-26 11:58:19 +0200

    Final Review

--- nurserySystem-main/Route/teacherRoute.js
+++ nurserySystem-main/Route/teacherRoute.js
@@ -2,26 +2,66 @@ const express = require('express');
 const teacherController = require('./../Controller/teacherController');
 const validateMW = require('./../Core/Validation/validateMW');
 const validateTeacher = require('./../Core/Validation/validateTeacher');
+const {
+  checkAdmin,
+  checkAdminOrTeacher,
+} = require('./../Core/Authentication/authenticationMW');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
+
+const uploadImg = multer({
+  fileFilter: (req, file, callBack) => {
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg'
+    ) {
+      callBack(null, true);
+    } else {
+      callBack(new Error('Add a valid Image'));
+    }
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, callBack) => {
+      callBack(null, path.join(__dirname, '..', 'images', 'teacher'));
+    },
+    filename: (req, file, callBack) => {
+      let extension = path.extname(file.originalname);
+      let fileName = path.basename(file.originalname, extension);
+      let unqImgName =
+        file.fieldname + '-' + fileName + '-' + Date.now() + extension;
+      callBack(null, unqImgName);
+    },
+  }),
+});
+
+const setImage = (req, res, next) => {
+  if (req.file && req.file.path) req.body.image = req.file.path;
+  next();
+};
+// Router
 router
   .route('/teachers')
-  .get(teacherController.getAllTeachers)
+  .get(checkAdmin, teacherController.getAllTeachers)
   .post(
+    uploadImg.single('image'),
+    setImage,
+    checkAdmin,
     validateTeacher.validateTeacherArray,
     validateMW,
     teacherController.addTeacher
   )
   .patch(
-    validateTeacher.optValidateTeacherArray,
-    validateMW,
-    teacherController.updateTeacher
-  )
-  .put(
+    uploadImg.single('image'),
+    setImage,
+    checkAdminOrTeacher,
     validateTeacher.optValidateTeacherArray,
     validateMW,
     teacherController.updateTeacher
   )
   .delete(
+    checkAdmin,
     validateTeacher.optValidateTeacherArray,
     validateMW,
     teacherController.deleteTeacher
